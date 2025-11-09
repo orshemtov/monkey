cd0d6f318f7fe4ad9afce11d9c921eb74ccf7461
Or Shemtov <oriesh@icloud.com>
2025-11-09 23:12:38 +0200

    added errors to parser

--- parser.go
+++ parser.go
@@ -1,19 +1,29 @@
 package main
 
+import "fmt"
+
 type Parser struct {
 	l         *Lexer
 	curToken  Token
 	peekToken Token
+	errors    []string
 }
 
 func NewParser(l *Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
 }
 
 func (p *Parser) nextToken() {
@@ -59,11 +69,17 @@ func (p *Parser) peekTokenIs(t TokenType) bool {
 	return p.peekToken.Type == t
 }
 
+func (p *Parser) peekError(t TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
 func (p *Parser) expectPeek(t TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
--- parser_test.go
+++ parser_test.go
@@ -13,6 +13,7 @@ func TestLetStatements(t *testing.T) {
 	p := NewParser(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -36,6 +37,21 @@ func TestLetStatements(t *testing.T) {
 			}
 		})
 	}
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+
+	t.FailNow()
 }
 
 func testLetStatement(t *testing.T, s Statement, name string) bool {
