d56a5c5971242eab49df5ab50bd6f9e45d6c54d5
Or Shemtov <oriesh@icloud.com>
2025-11-15 22:52:19 +0200

    added compiler and bytecode instructions, tests

--- code/code_test.go
+++ code/code_test.go
@@ -26,3 +26,58 @@ func TestMake(t *testing.T) {
 		})
 	}
 }
+
+func TestInstructionsString(t *testing.T) {
+	instructions := []Instructions{
+		Make(OpConstant, 1),
+		Make(OpConstant, 2),
+		Make(OpConstant, 65535),
+	}
+
+	expected := `0000 OpConstant 1
+0003 OpConstant 2
+0006 OpConstant 65535
+`
+
+	concatted := Instructions{}
+	for _, ins := range instructions {
+		concatted = append(concatted, ins...)
+	}
+
+	if concatted.String() != expected {
+		t.Errorf("instructions string wrong. expected=\n%q\ngot=\n%q", expected, concatted.String())
+	}
+}
+
+func TestReadOperands(t *testing.T) {
+	testCases := []struct {
+		op        Opcode
+		operands  []int
+		bytesRead int
+	}{
+		{
+			OpConstant, []int{65535}, 2,
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(string(tC.op), func(t *testing.T) {
+			instruction := Make(tC.op, tC.operands...)
+
+			def, err := Lookup(byte(tC.op))
+			if err != nil {
+				t.Fatalf("definition not found: %q\n", err)
+			}
+
+			operandsRead, n := ReadOperands(def, instruction[1:])
+			if n != tC.bytesRead {
+				t.Fatalf("n wrong. want=%d, got=%d", tC.bytesRead, n)
+			}
+
+			for i, want := range tC.operands {
+				if operandsRead[i] != want {
+					t.Errorf("operand wrong. want=%d, got=%d", want, operandsRead[i])
+				}
+			}
+		})
+	}
+}
---
+++ compiler/compiler_test.go
@@ -0,0 +1,120 @@
+package compiler
+
+import (
+	"fmt"
+	"testing"
+
+	"monkey/ast"
+	"monkey/code"
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+)
+
+func TestIntegerArithmetric(t *testing.T) {
+	testCases := []struct {
+		input                string
+		expectedConstants    []any
+		expectedInstructions []code.Instructions
+	}{
+		{
+			input:             "1 + 2",
+			expectedConstants: []any{1, 2},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+			},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.input, func(t *testing.T) {
+			program := parse(tC.input)
+
+			compiler := New()
+			err := compiler.Compile(program)
+			if err != nil {
+				t.Fatalf("compiler error: %s", err)
+			}
+
+			bytecode := compiler.Bytecode()
+			err = testInstructions(tC.expectedInstructions, bytecode.Instructions)
+			if err != nil {
+				t.Fatalf("instructions test failed: %s", err)
+			}
+
+			err = testConstants(tC.expectedConstants, bytecode.Constants)
+			if err != nil {
+				t.Fatalf("constants test failed: %s", err)
+			}
+		})
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testInstructions(
+	expected []code.Instructions,
+	actual code.Instructions,
+) error {
+	concatted := concatInstructions(expected)
+
+	if len(actual) != len(concatted) {
+		return fmt.Errorf("wrong instruction length: expected=%q, got=%q", concatted, actual)
+	}
+
+	for i, ins := range concatted {
+		if actual[i] != ins {
+			return fmt.Errorf("wrong instruction at %d: expected=%q, got=%q", i, ins, actual[i])
+		}
+	}
+
+	return nil
+}
+
+func concatInstructions(s []code.Instructions) code.Instructions {
+	out := code.Instructions{}
+
+	for _, ins := range s {
+		out = append(out, ins...)
+	}
+
+	return out
+}
+
+func testConstants(
+	expected []any,
+	actual []object.Object,
+) error {
+	if len(expected) != len(actual) {
+		return fmt.Errorf("wrong number of constants: expected=%d, got=%d", len(expected), len(actual))
+	}
+
+	for i, constant := range expected {
+		switch constant := constant.(type) {
+		case int:
+			err := testIntegerObject(int64(constant), actual[i])
+			if err != nil {
+				return fmt.Errorf("constant %d - testIntegerObject failed: %s", i, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. got=%d, want=%d", result.Value, expected)
+	}
+
+	return nil
+}
